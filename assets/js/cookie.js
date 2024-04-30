01fbe0791a5c47dce52a0f5e09f90d24adefa452
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2024-04-30 13:42:50 +0100

    cookie error

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -4,6 +4,8 @@
 $(window).on('load', function() {
     if (document.cookie.indexOf("accepted_cookies=") < 0) {
         $('.cookie-overlay').addClass("show");
+    } else {
+      $('.cookie-overlay').addClass("d-none");
     }
   
     $('.accept-cookies').on('click', function() {
