c7c283a3a242f458be701eea9e904ed09eda8762
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2024-07-09 11:34:22 +0100

    schema

--- assets/inc/_head.php
+++ assets/inc/_head.php
@@ -1,21 +1,25 @@
-
-
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-<title><?=$meta_title;?></title>
-<meta name="description" content="<?=$meta_desc;?>">
-<link rel="canonical" href="<?=$page_url;?>">
-<meta property="og:url" content="<?=$page_url;?>">
+<title><?= $meta_title; ?></title>
+<meta name="description" content="<?= $meta_desc; ?>">
+<link rel="canonical" href="<?= $page_url; ?>">
+<meta property="og:url" content="<?= $page_url; ?>">
 <meta property="og:type" content="website">
 <meta name="og:type" content="website">
 <meta property="og:image" content="https://www.bevtecltd.com/assets/img/og-img.png">
-<meta property="og:description" content="<?=$meta_desc;?>">
+<meta property="og:description" content="<?= $meta_desc; ?>">
 <script type="application/ld+json">
     {
         "@context": "http://schema.org",
-        "@type": "WebSite",
+        "@type": "LocalBusiness",
         "name": "Bevtec Ltd",
-        "url": "https://www.bevtecltd.com"
+        "description": "Specialised engineering support to the beverage industry. We are based in Long Sutton, Lincolnshire. Our services include: shift cover, specialist training, project management and many more."
+        "url": "https://www.bevtecltd.com",
+        "telephone": "07881 782704",
+        "openingHours": [
+            "Mo-fr 09:00-17:00"
+        ],
+
     }
 </script>
 <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="assets/img/apple-touch-icon.png">
--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -5,12 +5,8 @@ $(window).on('load', function() {
     if (document.cookie.indexOf("accepted_cookies=") < 0) {
         $('.cookie-overlay').removeClass("d-none");
     }
-  
     $('.accept-cookies').on('click', function() {
       document.cookie = "accepted_cookies=yes;"
       
     })
-  
-   
- 
 })
